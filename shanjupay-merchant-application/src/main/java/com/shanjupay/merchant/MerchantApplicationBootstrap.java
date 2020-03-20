0d6e7d2f93c7f3caeb6d203846a938d772e33c15
zfx1101804091 <1875586562@qq.com>
2020-03-20 18:51:45 +0800

    搭建商户平台应用工程

---
+++ shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/MerchantApplicationBootstrap.java
@@ -0,0 +1,17 @@
+package com.shanjupay.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: zheng-fx
+ * @time: 2020/3/20 0020 18:36
+ */
+@SpringBootApplication
+public class MerchantApplicationBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApplicationBootstrap.class,args);
+    }
+}
